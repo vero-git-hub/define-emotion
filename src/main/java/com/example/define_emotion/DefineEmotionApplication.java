3b0d76c220e3bf98eba7cb77752d085eba55c90e
v_code <12273445-v_code@users.noreply.gitlab.com>
2024-12-20 15:12:18 +0200

    Initial commit

---
+++ src/main/java/com/example/define_emotion/DefineEmotionApplication.java
@@ -0,0 +1,13 @@
+package com.example.define_emotion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DefineEmotionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DefineEmotionApplication.class, args);
+	}
+
+}
