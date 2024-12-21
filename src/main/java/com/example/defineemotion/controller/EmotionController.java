219347b2265c631d001f55158f965a642d8b0bc5
v_code <12273445-v_code@users.noreply.gitlab.com>
2024-12-21 11:22:24 +0200

    Home page

--- src/main/java/com/example/defineemotion/controller/EmotionController.java
+++ src/main/java/com/example/defineemotion/controller/EmotionController.java
@@ -3,18 +3,16 @@ package com.example.defineemotion.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 import com.example.defineemotion.dto.EmotionRequestDto;
 import com.example.defineemotion.dto.EmotionResponseDto;
 import com.example.defineemotion.service.EmotionService;
 
-@RestController
-@RequestMapping("/emotions")
+@Controller
 public class EmotionController {
 
     @Autowired
@@ -24,12 +22,17 @@ public class EmotionController {
         this.emotionService = emotionService;
     }
 
+    @GetMapping("/")
+    public String showHomePage() {
+        return "index";
+    }
+
     @PostMapping
     public EmotionResponseDto addEmotion(@RequestBody EmotionRequestDto emotionRequestDto) {
         return emotionService.addEmotion(emotionRequestDto.text(), emotionRequestDto.mood());
     }
 
-    @GetMapping
+    @GetMapping("/emotions")
     public List<EmotionResponseDto> getAllEmotions() {
         return emotionService.getAllEmotions();
     }
