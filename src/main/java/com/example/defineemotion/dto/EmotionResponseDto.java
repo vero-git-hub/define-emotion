aaa6de7f7650d93955af4f27c35922cfd36300a4
v_code <12273445-v_code@users.noreply.gitlab.com>
2025-01-03 15:00:49 +0200

    Date format

--- src/main/java/com/example/defineemotion/dto/EmotionResponseDto.java
+++ src/main/java/com/example/defineemotion/dto/EmotionResponseDto.java
@@ -1,7 +1,10 @@
 package com.example.defineemotion.dto;
 
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 public record EmotionResponseDto(Long id, String text, String mood, LocalDateTime timestamp) {
-
+    public String formattedTimestamp() {
+        return timestamp.format(DateTimeFormatter.ofPattern("MM-dd-yyyy"));
+    }
 }
